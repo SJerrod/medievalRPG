b2244ee7b6653d8777a68801afc84023850d1e52
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-03 13:19:28 -0800

    Create front end with adjustable stats
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/js/medievalRPG.js
+++ src/js/medievalRPG.js
@@ -1,34 +1,36 @@
-let warrior = {strength: 10, defence: 8, intelligence: 2, speed: 5};
-let paladin =  {strength: 8, defence: 10, intelligence: 5, speed: 2};
-let wizard = {strength: 2, defence: 5, intelligence: 10, speed: 8};
-let ranger = {strength: 5, defence: 2, intelligence: 8, speed: 10};
-let initialTarget = {strength: 2, defence: 2, intelligence: 2, speed: 2};
-
-// strengthTraining(5)(character)  improves characters strength by 5
-const strengthTraining = changeStats("strength")(5);
-const defenceTraining = changeStats("defence")(5);
-const intelligenceTraining = changeStats("intelligence")(5);
-const speedTraining = changeStats("speed")(5);
-
-
-const buffPlayerStrength = targetControl(strengthTraining)
-
-const storeTarget = (initialTarget) => {
+export const storeTarget = (initialTarget) => {
   let currentTarget = initialTarget;
   return (targetChangeFunction = target => target) => {
     const newTarget = targetChangeFunction(currentTarget);
     currentTarget = {...newTarget};
     return newTarget;
-  } 
-}
+  };
+};
 
-const targetControl = storeTarget();
+export const targetControl = storeTarget({strength: 0, defence: 0, intelligence: 0, speed: 0});
 
-const changeStats = (prop) => {
+export const changeStats = (prop) => {
   return (value) => {
     return (target) => ({
       ...target,
       [prop] : (target[prop] || 0) + value
-    })
-  }
-}
+    });
+  };
+};
+
+// let warrior = {strength: 10, defence: 8, intelligence: 2, speed: 5};
+// let paladin =  {strength: 8, defence: 10, intelligence: 5, speed: 2};
+// let wizard = {strength: 2, defence: 5, intelligence: 10, speed: 8};
+// let ranger = {strength: 5, defence: 2, intelligence: 8, speed: 10};
+// let initialTarget = {strength: 2, defence: 2, intelligence: 2, speed: 2};
+
+// strengthTraining(5)(character)  improves characters strength by 5
+export const strengthTraining = changeStats("strength")(5);
+export const defenceTraining = changeStats("defence")(5);
+export const intelligenceTraining = changeStats("intelligence")(5);
+export const speedTraining = changeStats("speed")(5);
+
+// const buffPlayerStrength = targetControl(strengthTraining)
+// const buffPlayerDefence = targetControl(defenceTraining)
+// const buffPlayerIntelligence = targetControl(intelligenceTraining)
+// const buffPlayerSpeed = targetControl(speedTraining)
--- src/main.js
+++ src/main.js
@@ -2,7 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {targetControl, strengthTraining, defenceTraining, intelligenceTraining, speedTraining} from './js/medievalRPG.js';
 
 $(document).ready(function() {
-  event.preventDefault();
+  $('#strengthBuff').click(function() {
+    const newTarget = targetControl(strengthTraining);
+    $('#strength-stat').text(`Strength: ${newTarget.strength}`);
+    console.log(newTarget);
+  });
+
+  $('#defenceBuff').click(function() {
+    const newTarget = targetControl(defenceTraining);
+    $('#defence-stat').text(`Defence: ${newTarget.defence}`);
+  });
+
+  $('#intelligenceBuff').click(function() {
+    const newTarget = targetControl(intelligenceTraining);
+    $('#intelligence-stat').text(`Intelligence: ${newTarget.intelligence}`);
+  });
+
+  $('#speedBuff').click(function() {
+    const newTarget = targetControl(speedTraining);
+    $('#speed-stat').text(`Speed: ${newTarget.speed}`);
+  });
+
+  $('#show-target').click(function() {
+    const currentTarget = targetControl();
+    $('#strength-stat').text(currentTarget.strength);
+    $('#defence-stat').text(currentTarget.defence);
+    $('#intelligence-stat').text(currentTarget.intelligence);
+    $('#speed-stat').text(currentTarget.speed);
+  });
 });
