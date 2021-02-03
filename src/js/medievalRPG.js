ac9a2366e4d7d2da150249b3e001828ee85c45b6
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-03 11:18:25 -0800

    Create curried function to mutate shallow copy of target object
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/js/medievalRPG.js
+++ src/js/medievalRPG.js
@@ -1,38 +1,34 @@
-class Character {
-  constructor(name) {
-    this.name = name;
-    this.health = 100;
-    this.strength = 0;
-    this.defence = 0;
-    this.intelligence = 0;
-    this.speed = 0;
-  }
+let warrior = {strength: 10, defence: 8, intelligence: 2, speed: 5};
+let paladin =  {strength: 8, defence: 10, intelligence: 5, speed: 2};
+let wizard = {strength: 2, defence: 5, intelligence: 10, speed: 8};
+let ranger = {strength: 5, defence: 2, intelligence: 8, speed: 10};
+let initialTarget = {strength: 2, defence: 2, intelligence: 2, speed: 2};
+
+// strengthTraining(5)(character)  improves characters strength by 5
+const strengthTraining = changeStats("strength")(5);
+const defenceTraining = changeStats("defence")(5);
+const intelligenceTraining = changeStats("intelligence")(5);
+const speedTraining = changeStats("speed")(5);
+
+
+const buffPlayerStrength = targetControl(strengthTraining)
+
+const storeTarget = (initialTarget) => {
+  let currentTarget = initialTarget;
+  return (targetChangeFunction = target => target) => {
+    const newTarget = targetChangeFunction(currentTarget);
+    currentTarget = {...newTarget};
+    return newTarget;
+  } 
 }
 
-const strengthTraining = (character) => {
-  return {
-    ...character,
-    strength: (character.strength || 0) + 1
-  }
-};
+const targetControl = storeTarget();
 
-const defenceTraining = (character) => {
-  return {
-    ...character,
-    defence: (character.defence || 0) + 1
+const changeStats = (prop) => {
+  return (value) => {
+    return (target) => ({
+      ...target,
+      [prop] : (target[prop] || 0) + value
+    })
   }
-};
-
-const intelligenceTraining = (character) => {
-  return {
-    ...character,
-    intelligence: (character.intelligence || 0) + 1
-  }
-};
-
-const speedTraining = (character) => {
-  return {
-    ...character,
-    speed: (character.speed || 0) + 1
-  }
-};
+}
