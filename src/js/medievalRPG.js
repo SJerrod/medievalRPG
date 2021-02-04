35e121b15035e48858fa003c473ca1d5aaa1bef3
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-03 16:20:05 -0800

    Work to add attack function
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/js/medievalRPG.js
+++ src/js/medievalRPG.js
@@ -8,6 +8,10 @@ export const storeTarget = (initialTarget) => {
 };
 
 export const targetControl = storeTarget({strength: 0, defence: 0, intelligence: 0, speed: 0});
+// let warrior = {strength: 10, defence: 8, intelligence: 2, speed: 5};
+// let paladin =  {strength: 8, defence: 10, intelligence: 5, speed: 2};
+// let wizard = {strength: 2, defence: 5, intelligence: 10, speed: 8};
+// let ranger = {strength: 5, defence: 2, intelligence: 8, speed: 10};
 
 export const changeStats = (prop) => {
   return (value) => {
@@ -17,12 +21,6 @@ export const changeStats = (prop) => {
     });
   };
 };
-
-// let warrior = {strength: 10, defence: 8, intelligence: 2, speed: 5};
-// let paladin =  {strength: 8, defence: 10, intelligence: 5, speed: 2};
-// let wizard = {strength: 2, defence: 5, intelligence: 10, speed: 8};
-// let ranger = {strength: 5, defence: 2, intelligence: 8, speed: 10};
-// let initialTarget = {strength: 2, defence: 2, intelligence: 2, speed: 2};
 
 // strengthTraining(5)(character)  improves characters strength by 5
 export const strengthTraining = changeStats("strength")(5);
@@ -34,3 +32,57 @@ export const speedTraining = changeStats("speed")(5);
 // const buffPlayerDefence = targetControl(defenceTraining)
 // const buffPlayerIntelligence = targetControl(intelligenceTraining)
 // const buffPlayerSpeed = targetControl(speedTraining)
+
+// composition
+const buffTarget = (target) => {
+  const obj = {
+    buff: (train) => {
+      return `The ${target.name || target} improved its ${train} stats.`;
+    }
+  };
+  return obj;
+};
+
+// examples
+// const character = buffTarget("character");
+// character.buff("strength");
+// buffTarget("foobar") makes: foobar { buff() }  foobar.buff("baz")
+
+// composition
+// const hitTarget = (target) => {
+//   const obj = {
+//     hit: () => {
+//       target.health -= 10;
+//       return `The ${target.name || target} has been hit!`;
+//     }
+//   };
+//   return obj;
+// };
+
+const hitTarget = (target) => {
+  const obj = {
+    hit: () => {
+      const minusHealth = {
+        health: target.health - 10
+      }
+      return target = {...target, ...minusHealth}
+    }
+  };
+  return obj;
+};
+
+//examples
+// const enemy = hitTarget("enemy");
+// enemy.hit();
+
+// function factory
+export const buffHitTarget = (name) => {
+  let target = {};
+    target.name = name;
+    target.health = 100;
+  return { ...target, ...buffTarget(target), ...hitTarget(target) };
+};
+
+// examples
+// const player = buffHitTarget("player");
+// console.log(player.buff("strength"));
--- src/main.js
+++ src/main.js
@@ -2,32 +2,45 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {targetControl, strengthTraining, defenceTraining, intelligenceTraining, speedTraining} from './js/medievalRPG.js';
+import {targetControl, buffHitTarget, strengthTraining, defenceTraining, intelligenceTraining, speedTraining} from './js/medievalRPG.js';
 
 $(document).ready(function() {
   $('#strengthBuff').click(function() {
     const newTarget = targetControl(strengthTraining);
+    const target = buffHitTarget("player");
     $('#strength-stat').text(`Strength: ${newTarget.strength}`);
-    console.log(newTarget);
+    $('#current-action').text(target.buff("strength"));
   });
 
   $('#defenceBuff').click(function() {
     const newTarget = targetControl(defenceTraining);
+    const target = buffHitTarget("player");
     $('#defence-stat').text(`Defence: ${newTarget.defence}`);
+    $('#current-action').text(target.buff("defense"));
   });
 
   $('#intelligenceBuff').click(function() {
     const newTarget = targetControl(intelligenceTraining);
+    const target = buffHitTarget("player");
     $('#intelligence-stat').text(`Intelligence: ${newTarget.intelligence}`);
+    $('#current-action').text(target.buff("intelligence"));
   });
 
   $('#speedBuff').click(function() {
     const newTarget = targetControl(speedTraining);
+    const target = buffHitTarget("player");
     $('#speed-stat').text(`Speed: ${newTarget.speed}`);
+    $('#current-action').text(target.buff("speed"));
+  });
+
+  $('#attack').click(function() {
+    const target = buffHitTarget("target");
+    $('#current-action').text(target.hit("speed"));
   });
 
   $('#show-target').click(function() {
     const currentTarget = targetControl();
+    $('#current-action').text("");
     $('#strength-stat').text(currentTarget.strength);
     $('#defence-stat').text(currentTarget.defence);
     $('#intelligence-stat').text(currentTarget.intelligence);
